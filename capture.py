e9ac68e553a6bc4f3d245f22ba7d683f225e71fc
Nick Bild <nick.bild@gmail.com>
2020-04-05 23:03:34 +0100

    Start on training script.

--- capture.py
+++ capture.py
@@ -4,19 +4,32 @@ from PIL import Image
 import numpy as np
 
 
+folder = "train/up"
+start = 0
+end = 2
+
+
 with picamera.PiCamera() as camera:
     # Configure and warm up camera.
-    camera.color_effects = (128,128)
-    camera.resolution = (320,240)
+    camera.color_effects = (128, 128)
+    camera.resolution = (320, 240)
     camera.rotation = 180
     time.sleep(2)
 
-    # Capture a downsampled frame.
-    camera.capture('test.bmp', format='bmp', resize=(10, 10))
+    for cnt in range(start, end+1):
+        print("Capturing image: {}".format(cnt))
 
-    # Retrieve pixel data.
-    im = Image.open("test.bmp")
-    p = np.array(im)
+        # Capture a downsampled frame.
+        camera.capture('img/{}/image_{}.bmp'.format(folder, cnt), format='bmp', resize=(10, 10))
 
-    print(p)
+        # Retrieve pixel data.
+        im = Image.open('img/{}/image_{}.bmp'.format(folder, cnt))
+        p = np.array(im)
+
+        # Serialize and reduce dimensionality.
+        p = p.flatten()[0::3]
+        #print(p)
+
+        with open('{}/image_{}.txt'.format(folder, cnt), 'w') as f:
+            f.writelines("%s\n" % v for v in p)
 
