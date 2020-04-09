776774759ecf2028a6cb4a792ba0fe8117365f89
nickbild <Nicholas.Bild@sprint.com>
2020-04-08 20:40:53 -0400

    Add function to test classifier.

--- knn.py
+++ knn.py
@@ -1,14 +1,15 @@
 import glob
+import sys
 
 
 vectors = []
 labels = []
 
 
-def read_in_data():
-    files = glob.glob("train/up/image_*.txt")
+def load_training_data():
+    files = glob.glob("train/*/image_*.txt")
     for file in files:
-        label = "up"
+        label = file.split("/")[-2]
         with open(file, "r") as f:
             vector = []
             for line in f:
@@ -16,19 +17,38 @@ def read_in_data():
                 vector.append(line)
             vectors.append(vector)
             labels.append(label)
+    return
 
 
-def calculate_all_distances():
-    for i in range(len(vectors)-1):
-        for j in range(i+1, len(vectors)):
-            distance = 0
-            for k in range(len(vectors[i])):
-                distance += abs(vectors[i][k] - vectors[j][k])
-            print("{}-{}: {}".format(i, j, distance))
+def load_single_image(file):
+    vector = []
+
+    with open(file, "r") as f:
+        for line in f:
+            line = int(line.strip())
+            vector.append(line)
+
+    return vector
 
 
-read_in_data()
-print(vectors)
-print(labels)
+def calculate_distances_to_new_point(new_vector):
+    results = {}
 
-calculate_all_distances()
+    for i in range(len(vectors)):
+        distance = 0
+        for j in range(len(vectors[i])):
+            distance += abs(vectors[i][j] - new_vector[j])
+        if distance in results and results[distance] != labels[i]:
+            print("Warning: duplicate distance found and label not same!")
+        results[distance] = labels[i]
+
+    return results
+
+
+load_training_data()
+
+v = load_single_image(sys.argv[1])
+results = calculate_distances_to_new_point(v)
+
+for r in sorted(results.keys()):
+    print("{}: {}".format(r, results[r]))
