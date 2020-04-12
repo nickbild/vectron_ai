1f02ce4f5f63d7b527b6438f19a9ee73283a2d5e
nickbild <Nicholas.Bild@sprint.com>
2020-04-12 08:59:23 -0400

    Update interaction with Vectron 64.

--- capture_vectron.py
+++ capture_vectron.py
@@ -7,6 +7,28 @@ import picamera
 import time
 from PIL import Image
 import numpy as np
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BOARD)
+
+delay_time = 0.000001
+
+data = 8
+latch = 10
+clock = 12
+
+interrupt = 16
+interrupt_clear = 18
+
+# Shift register.
+GPIO.setup(data, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(latch, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(clock, GPIO.OUT, initial=GPIO.LOW)
+
+# Vectron 64 communication.
+GPIO.setup(interrupt, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(interrupt_clear, GPIO.IN)
 
 
 def threshold(v):
@@ -24,6 +46,14 @@ def threshold(v):
         v = 5
 
     return v
+
+
+def push_to_sr(v):
+    GPIO.output(data, v)
+    GPIO.output(clock, GPIO.HIGH)
+    time.sleep(delay_time)
+    GPIO.output(clock, GPIO.LOW)
+    time.sleep(delay_time)
 
 
 with picamera.PiCamera() as camera:
@@ -46,13 +76,25 @@ with picamera.PiCamera() as camera:
         # Serialize and reduce dimensionality.
         p = p.flatten()[0::3]
 
+        # For each byte...
         for v in p:
+            # Downscale value and convert to binary string.
             v = threshold(v)
-            print(v)
             bin = '{0:08b}'.format(v)
-            print(bin)
-            for bit in bin: # MSB first.
-                print(bit)
-            print("----")
 
-        time.sleep(5)
+            # Write each bit to shift register.
+            for bit in bin: # MSB -> Qh.
+                push_to_sr(int(bit))
+
+            # Latch the byte.
+            GPIO.output(latch, GPIO.HIGH)
+            time.sleep(delay_time)
+            GPIO.output(latch, GPIO.LOW)
+
+            # Send interrupt to Vectron 64.
+            GPIO.output(interrupt, GPIO.LOW)
+            time.sleep(0.00002) # 20 microseconds
+            GPIO.output(interrupt, GPIO.HIGH)
+
+            # Wait for interrupt to clear.
+            GPIO.wait_for_edge(interrupt_clear, GPIO.FALLING)
